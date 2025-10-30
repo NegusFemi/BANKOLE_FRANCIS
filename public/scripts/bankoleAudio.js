f14997575600999ce346618f947e7fe9f19e47de
Otuyemi Oluwafemi <otuyemifemi@gmail.com>
2025-10-31 00:08:50 +0100

    17th commit and the first in about a week. putting finishing touches to the website

--- public/scripts/bankoleAudio.js
+++ public/scripts/bankoleAudio.js
@@ -9,35 +9,20 @@ icons.forEach((iconElement, index) => {
         if (!songElement.paused) {
             // Pause the clicked song if it is playing
             songElement.pause();
-            iconElement.innerHTML = '<i class="fa-regular fa-circle-play" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem !important; cursor: pointer; margin-top: 1rem !important;"></i>';
+            iconElement.innerHTML = '<i class="fa-regular fa-circle-play" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem !important; cursor: pointer; margin-top: 1rem;"></i>';
         } else {
             // Pause all other songs and reset their icons
             songs.forEach((s, i) => {
                 if (!s.paused) {
                     s.pause();
-                    icons[i].innerHTML = '<i class="fa-regular fa-circle-play" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem !important; cursor: pointer; margin-top: 1rem !important;"></i>';
+                    icons[i].innerHTML = '<i class="fa-regular fa-circle-play" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem !important; cursor: pointer; margin-top: 1rem !important; position: relative;"></i>';
                 }
             });
             // Play the selected song and update its icon
+            const isMobile = window.matchMedia('(max-width: 37.5em)').matches;
+            const pauseMargin = isMobile ? '-0.5rem !important' : '1rem !important';
             songElement.play();
-            iconElement.innerHTML = '<i class="fa-regular fa-circle-pause" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem; cursor: pointer; margin-top: 1rem !important;"></i>';
+            iconElement.innerHTML = `<i class="fa-regular fa-circle-pause" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem; cursor: pointer; margin-top: ${pauseMargin}; position: relative !important; z-index: 9999 !important"></i>`;
         }
     });
 });
-
-
-
-
-/*cons.forEach((iconElement, index) => {
-    iconElement.addEventListener('click', () => {
-        const songElement = songs[index];
-        if (songElement.paused) {
-            songElement.play();
-            iconElement.innerHTML = '<i class="fa-regular fa-circle-pause" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem; cursor: pointer; margin-top: 1rem !important;"></i>';
-        } else {
-            songElement.pause();
-            iconElement.innerHTML = '<i class="fa-regular fa-circle-play" style="color: rgba(4, 33, 39, 0.952); font-size: 6rem !important; cursor: pointer; margin-top: 1rem !important;"></i>';
-        }
-    });
-});
-*/
